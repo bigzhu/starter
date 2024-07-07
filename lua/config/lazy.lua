1a80bdbe4b1f3250f3a02567a7810afb7aaf5fb8
bigzhu <vermiliondun@gmail.com>
2024-07-07 10:15:20 +0800

    snip

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.coding.mini-comment" },
+    { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "plugins" },
   },
   defaults = {
---
+++ lua/plugins/LuaSnip.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    lazy = false,
+    -- follow latest release.
+    version = "2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
+    config = function()
+      -- load snippets paths
+      require("luasnip.loaders.from_vscode").lazy_load({
+        paths = {
+          "./snippets/vscode-python",
+          "./snippets/flutter-riverpod-snippets",
+          "./snippets/awesome-flutter-snippets",
+        },
+      })
+    end,
+  },
+}
