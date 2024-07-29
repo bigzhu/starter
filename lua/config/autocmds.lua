2caebbb40b618fdb482dd5c33120c3e7c4ab13b5
bigzhu <vermiliondun@gmail.com>
2024-07-29 23:49:03 +0800

    把光标改回 |

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- https://github.com/neovim/neovim/issues/4396#issuecomment-1377191592 提供了这个方法
+-- vim.cmd('autocmd VimLeave * set guicursor= | call chansend(v:stderr, "\\e[5 q")')
+-- 把光标改回 |
+vim.api.nvim_create_autocmd({ "VimLeave" }, {
+  command = 'set guicursor= | call chansend(v:stderr, "\\e[5 q")',
+})
