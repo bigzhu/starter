1ce2818e3a6febbef8b51b8010f109412bf03a67
bigzhu <vermiliondun@gmail.com>
2024-07-30 17:47:37 +0800

    cursor

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,6 +5,7 @@
 -- https://github.com/neovim/neovim/issues/4396#issuecomment-1377191592 提供了这个方法
 -- vim.cmd('autocmd VimLeave * set guicursor= | call chansend(v:stderr, "\\e[5 q")')
 -- 把光标改回 |
+-- 没有用, 只要再开 tmux tab, 就是柱子
 vim.api.nvim_create_autocmd({ "VimLeave" }, {
   command = 'set guicursor= | call chansend(v:stderr, "\\e[5 q")',
 })
