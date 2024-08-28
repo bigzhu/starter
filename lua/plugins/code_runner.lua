eb7372ad9e1f72396faf87cdde3e20817b5976da
bigzhu <vermiliondun@gmail.com>
2024-08-28 11:12:55 +0800

    go run

--- lua/plugins/code_runner.lua
+++ lua/plugins/code_runner.lua
@@ -4,6 +4,7 @@ return {
     config = function()
       require("code_runner").setup({
         filetype = {
+          go = "go test $dir",
           dart = "flutter test",
           python = "poetry run python",
           markdown = 'open "obsidian://open?file=$fileNameWithoutExt"',
