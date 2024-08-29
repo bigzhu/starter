15b9e06eb41f55bf51f4692f791b265469ef83f2
bigzhu <vermiliondun@gmail.com>
2024-08-29 22:28:11 +0800

    feat: config for avante

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,7 +5,7 @@ vim.keymap.set("n", "<C-f>", "<cmd>ObsidianQuickSwitch<CR>", { desc = "Search ch
 vim.keymap.set("n", "<C-j>", "<cmd>ObsidianSearch<CR>", { desc = "Search cheese by content" })
 vim.keymap.set("n", "<C-g>", "<cmd>ObsidianToday<CR>", { desc = "Cheese create a new daily note" })
 -- 进入 cheese 的 todo 页面
-vim.keymap.set("n", "<leader>td", "<cmd>TodoTelescope cwd=~/Sync/home/cheese/<CR>", { desc = "Enter cheese todo" })
+-- vim.keymap.set("n", "<leader>td", "<cmd>TodoTelescope cwd=~/Sync/home/cheese/<CR>", { desc = "Enter cheese todo" })
 vim.keymap.set("n", "<leader>r", ":RunCode<CR>", { desc = "Run current code" })
 
 --  make nvim distinguish between <CR> (Enter) and <C-CR> (Ctrl+Enter)
--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -1,13 +1,27 @@
 -- lazy 的配置, 可以参看  example.lua
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  {
+    "ellisonleao/gruvbox.nvim",
+    init = function()
+      vim.opt.background = "dark" -- or "light" if you want light mode
+    end,
+  },
+  {
+    "eddyekofo94/gruvbox-flat.nvim",
+    priority = 1000,
+    enabled = true,
+    config = function()
+      vim.cmd([[colorscheme gruvbox-flat]])
+      vim.g.gruvbox_flat_style = "dark"
+    end,
+  },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "gruvbox-flat",
     },
   },
 }
