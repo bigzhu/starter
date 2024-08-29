5e81761bfffc3ea1bfa0a518f4bcbbc51112877c
bigzhu <vermiliondun@gmail.com>
2024-08-29 20:41:15 +0800

    feat: the nvim curor work fine

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,5 +6,10 @@ vim.keymap.set("n", "<C-j>", "<cmd>ObsidianSearch<CR>", { desc = "Search cheese 
 vim.keymap.set("n", "<C-g>", "<cmd>ObsidianToday<CR>", { desc = "Cheese create a new daily note" })
 -- 进入 cheese 的 todo 页面
 vim.keymap.set("n", "<leader>td", "<cmd>TodoTelescope cwd=~/Sync/home/cheese/<CR>", { desc = "Enter cheese todo" })
-
 vim.keymap.set("n", "<leader>r", ":RunCode<CR>", { desc = "Run current code" })
+
+--  make nvim distinguish between <CR> (Enter) and <C-CR> (Ctrl+Enter)
+vim.api.nvim_set_keymap("", "<Esc>[13;5u", "<C-CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("!", "<Esc>[13;5u", "<C-CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "<Esc>[13;2u", "<S-CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("!", "<Esc>[13;2u", "<S-CR>", { noremap = true, silent = true })
