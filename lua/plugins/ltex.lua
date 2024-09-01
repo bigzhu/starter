035dd6ba6ed73e02b66dca80a6fd6bf6de890992
bigzhu <vermiliondun@gmail.com>
2024-09-01 09:53:25 +0800

    chore: ltex check English sentence

---
+++ lua/plugins/ltex.lua
@@ -0,0 +1,32 @@
+-- 句子和拼写检查
+-- mason need install ltex-ls
+-- https://medium.com/@Erik_Krieg/free-and-open-source-grammar-correction-in-neovim-using-ltex-and-n-grams-dea9d10bc964
+return {
+  "barreiroleo/ltex_extra.nvim",
+  ft = { "markdown", "tex", "dart", "go" },
+  dependencies = { "neovim/nvim-lspconfig" },
+
+  config = function()
+    require("ltex_extra").setup({
+      -- your_ltex_extra_opts,
+      server_opts = {
+        -- capabilities = your_capabilities,
+        on_attach = function(client, bufnr)
+          -- your on_attach process
+        end,
+        settings = {
+          ltex = {
+            -- motherTounge = "en",
+            filetypes = { "markdown", "tex", "dart", "go" },
+            language = "en",
+            -- additionalRules = {
+            --   enablePickyRules = true,
+            --   languageModel = "/Users/bigzhu/Sync/config/nvim/models/",
+            --   checkFrequency = "save",
+            -- },
+          },
+        },
+      },
+    })
+  end,
+}
