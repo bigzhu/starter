ca7d3ff319c109203c50028c8d2626786e2d5838
bigzhu <vermiliondun@gmail.com>
2024-09-01 09:52:56 +0800

    chroe: tt to translate word

---
+++ lua/plugins/translate.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "uga-rosa/translate.nvim",
+    lazy = true,
+    config = true,
+    keys = {
+      { "tt", "viw<CMD>Translate ZH<CR>", desc = "Translate to Chinese", mode = { "n" } },
+    },
+  },
+}
